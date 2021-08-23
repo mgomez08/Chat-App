9e0634be54998f8197e001e2b92c977423e14192
Marlon Gómez <marlonjulian15@gmail.com>
2021-08-23 11:56:46 -0500

    Add element and styles for the circle status

--- src/components/Chat/ChatsSidebar/ChatItem.js
+++ src/components/Chat/ChatsSidebar/ChatItem.js
@@ -1,15 +1,21 @@
 import React from "react";
 import avatar from "../../../assets/avatar.jpg";
-export const ChatItem = ({ setIsSelect }) => {
+
+export const ChatItem = ({ setIsSelect, user }) => {
   const hola = () => {
     setIsSelect(true);
   };
   return (
     <div className="chat-item" onClick={hola}>
-      <img src={avatar} alt="avatar" className="img-friend" />
+      <div className="img-and-status">
+        <img src={avatar} alt="avatar" className="img-friend" />
+        <div
+          className={`circle-status ${user.online ? "online" : "offline"}`}
+        ></div>
+      </div>
       <div className="sumary">
         <div className="info-msg">
-          <p className="friend-name">Andrés Arturo Manzilla Vega</p>
+          <p className="friend-name">{user.name}</p>
           <span className="date-last-msg">7:33 p.m</span>
         </div>
         <div className="last-msg-container">
