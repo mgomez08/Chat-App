6919b1019d540a2e091fc6b2703844d215c5e4e0
Marlon Gómez <marlonjulian15@gmail.com>
2021-08-23 11:57:49 -0500

    Add ChatContext, chatReducer and use in the App

---
+++ src/context/chat/chatReducer.js
@@ -0,0 +1,14 @@
+import { types } from "../../types/types";
+
+export const chatReducer = (state, action) => {
+  switch (action.type) {
+    case types.loadUsers:
+      return {
+        ...state,
+        users: [...action.payload],
+      };
+
+    default:
+      return state;
+  }
+};
