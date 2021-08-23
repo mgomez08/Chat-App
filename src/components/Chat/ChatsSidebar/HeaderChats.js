57850c1b375082f49d1882b35a7d2601cb82a081
Marlon Gómez <marlonjulian15@gmail.com>
2021-08-22 20:34:24 -0500

    use Logout function

--- src/components/Chat/ChatsSidebar/HeaderChats.js
+++ src/components/Chat/ChatsSidebar/HeaderChats.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import avatar from "../../../assets/avatar.jpg";
+import { AuthContext } from "../../../auth/AuthContext";
+
 export const HeaderChats = () => {
+  const { auth, logout } = useContext(AuthContext);
   return (
     <div className="header-chats">
       <img src={avatar} alt="avatar" className="img-avatar" />
-      <h2 className="username">Marlon Gómez</h2>
+      <h2 className="username">{auth.name}</h2>
       <Link to="/auth/login">
-        <button className="button-logout">Salir</button>
+        <button className="button-logout" onClick={logout}>
+          Salir
+        </button>
       </Link>
     </div>
   );
