1a1544a52140241a39f92a1ecd921ba1449baf77
Marlon Gómez <marlonjulian15@gmail.com>
2021-08-23 11:58:18 -0500

    Listen users-list socket event

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -1,26 +1,42 @@
 import React, { useContext, useEffect } from "react";
 import { createContext } from "react";
+
 import { AuthContext } from "../auth/AuthContext";
+import { ChatContext } from "./chat/ChatContext";
+
 import { useSocket } from "../hooks/useSocket";
+
+import { types } from "../types/types";
 
 export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
   const { auth } = useContext(AuthContext);
+  const { dispatch } = useContext(ChatContext);
   const { socket, online, socketConnect, socketDisconnect } = useSocket(
     "http://localhost:8080"
   );
-
+  //userConnect
   useEffect(() => {
     if (auth.logged) {
       socketConnect();
     }
   }, [auth, socketConnect]);
+  //userDisconnect
   useEffect(() => {
     if (!auth.logged) {
       socketDisconnect();
     }
   }, [auth, socketDisconnect]);
+  //Listen to changes in connected users
+  useEffect(() => {
+    socket?.on("users-list", (users) => {
+      dispatch({
+        type: types.loadUsers,
+        payload: users,
+      });
+    });
+  }, [socket, dispatch]);
 
   return (
     <SocketContext.Provider value={{ socket, online }}>
