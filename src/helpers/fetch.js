45390802d22f17e1c8a180647f0c55169b62f84c
Marlon Gómez <marlonjulian15@gmail.com>
2021-08-22 20:32:56 -0500

    Add function fetchWithToken

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -2,7 +2,6 @@ const baseUrl = process.env.REACT_APP_API_URL;
 
 export const fetchWithoutToken = async (endpoint, data, method = "GET") => {
   const url = `${baseUrl}/${endpoint}`;
-  console.log(url);
   if (method === "GET") {
     const response = await fetch(url);
     return await response.json();
@@ -11,6 +10,29 @@ export const fetchWithoutToken = async (endpoint, data, method = "GET") => {
       method,
       headers: {
         "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    return await response.json();
+  }
+};
+
+export const fetchWithToken = async (endpoint, data, method = "GET") => {
+  const url = `${baseUrl}/${endpoint}`;
+  const token = localStorage.getItem("token") || "";
+  if (method === "GET") {
+    const response = await fetch(url, {
+      headers: {
+        "x-token": token,
+      },
+    });
+    return await response.json();
+  } else {
+    const response = await fetch(url, {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+        "x-token": token,
       },
       body: JSON.stringify(data),
     });
