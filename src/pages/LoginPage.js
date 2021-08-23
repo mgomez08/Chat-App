f33ccaecfaf5ce0dc52ab0b37b2318e5d38c182a
Marlon Gómez <marlonjulian15@gmail.com>
2021-08-22 20:34:04 -0500

    add functionality of login and register

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../auth/AuthContext";
+import Swal from "sweetalert2";
+import { useForm } from "../hooks/useForm";
 
 export const LoginPage = () => {
-  const submit = (e) => {
+  const { login } = useContext(AuthContext);
+  const [form, setForm, handleChange] = useForm({
+    email: "test@test.com",
+    password: "1231234",
+    rememberme: false,
+  });
+  useEffect(() => {
+    const email = localStorage.getItem("email");
+    if (email) {
+      setForm((form) => ({
+        ...form,
+        email,
+        rememberme: true,
+      }));
+    }
+    // eslint-disable-next-line
+  }, []);
+  const toggleCheck = () => {
+    setForm({
+      ...form,
+      rememberme: !form.rememberme,
+    });
+  };
+  const allFull = () => {
+    return form.email.length > 0 && form.password.length > 0 ? true : false;
+  };
+  const submit = async (e) => {
     e.preventDefault();
+    form.rememberme
+      ? localStorage.setItem("email", form.email)
+      : localStorage.removeItem("email", form.email);
+    const { email, password } = form;
+    const response = await login(email, password);
+    if (!response.ok) {
+      Swal.fire("Error", response.msg, "error");
+    }
   };
   return (
     <form className="form-auth" onSubmit={submit}>
@@ -11,24 +48,36 @@ export const LoginPage = () => {
       <input
         className="input-email"
         type="email"
+        name="email"
         placeholder="Correo electrónico"
+        value={form.email}
+        onChange={handleChange}
       />
       <input
         className="input-password"
         type="password"
+        name="password"
         placeholder="Contraseña"
+        value={form.password}
+        onChange={handleChange}
       />
       <div className="actions">
-        <div className="checkbox-container">
-          <input type="checkbox" id="remember" />
-          <label className="remember" for="remember">
+        <div className="checkbox-container" onClick={() => toggleCheck()}>
+          <input
+            name="rememberme"
+            type="checkbox"
+            id="rememberme"
+            checked={form.rememberme}
+            onChange={handleChange}
+          />
+          <label className="remember" htmlFor="remember">
             Recordarme
           </label>
         </div>
         <Link to="/auth/register">¿Nueva cuenta?</Link>
       </div>
-      <button className="button-auth">
-        <Link to="/">Ingresar</Link>
+      <button disabled={!allFull()} type="submit" className="button-auth">
+        Ingresar
       </button>
     </form>
   );
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,29 +1,63 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+import { AuthContext } from "../auth/AuthContext";
+import { useForm } from "../hooks/useForm";
 
 export const RegisterPage = () => {
-  const submit = (e) => {
+  const { register } = useContext(AuthContext);
+  const [form, , handleChange] = useForm({
+    name: "Tests",
+    email: "test@test.com",
+    password: "1231234",
+  });
+  const allFull = () => {
+    return form.name.length > 0 &&
+      form.email.length > 0 &&
+      form.password.length > 0
+      ? true
+      : false;
+  };
+  const submit = async (e) => {
     e.preventDefault();
+    const { name, email, password } = form;
+    const response = await register(name, email, password);
+    if (!response.ok) {
+      Swal.fire("Error", response.msg, "error");
+    }
   };
   return (
     <form className="form-auth" onSubmit={submit}>
       <h2 className="title-form">Register ChatApp</h2>
-      <input className="input-name" type="text" placeholder="Nombre" />
+      <input
+        className="input-name"
+        type="text"
+        placeholder="Nombre"
+        name="name"
+        value={form.name}
+        onChange={handleChange}
+      />
       <input
         className="input-email"
         type="email"
         placeholder="Correo electrónico"
+        name="email"
+        value={form.email}
+        onChange={handleChange}
       />
       <input
         className="input-password"
         type="password"
         placeholder="Contraseña"
+        name="password"
+        value={form.password}
+        onChange={handleChange}
       />
       <div className="actions flex-end">
         <Link to="/auth/login">¿Ya tienes una cuenta?</Link>
       </div>
-      <button className="button-auth">
-        <Link to="/auth/login">Registrarse</Link>
+      <button disabled={!allFull()} type="submit" className="button-auth">
+        Registrarse
       </button>
     </form>
   );
