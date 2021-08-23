ee10bd7b4e8221634faecdfdc0957f87aa337d41
Marlon Gómez <marlonjulian15@gmail.com>
2021-08-23 11:58:43 -0500

    Load users in the UI

--- src/components/Chat/ChatsSidebar/ChatsList.js
+++ src/components/Chat/ChatsSidebar/ChatsList.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../../../auth/AuthContext";
+import { ChatContext } from "../../../context/chat/ChatContext";
 import { ChatItem } from "./ChatItem";
 
 export const ChatsList = ({ setIsSelect }) => {
-  const chats = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-
+  const { chatState } = useContext(ChatContext);
+  const { auth } = useContext(AuthContext);
+  const users = chatState.users;
+  console.log(users);
   return (
     <div className="chats">
-      {chats.map((chat) => (
-        <ChatItem setIsSelect={setIsSelect} key={chat} />
-      ))}
+      {users.map((user, i) => {
+        if (user.uid !== auth.uid) {
+          return <ChatItem setIsSelect={setIsSelect} user={user} key={i} />;
+        }
+        return null;
+      })}
     </div>
   );
 };
